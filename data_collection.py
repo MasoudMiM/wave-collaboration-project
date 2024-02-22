ffc4eb109416d1f9b02f3a8c9b22ef1c8fbac037
Masoud Masoumi <masoumi.masoud@gmail.com>
2024-02-22 11:23:19 -0500

    first commit

---
+++ data_collection.py
@@ -0,0 +1,18 @@
+# This is the code for data collection from the link shapred
+# here is the link: https://onedrive.live.com/?authkey=%21AMogwvv2lAznbkw&id=94C083E5EBACF1E%214270&cid=094C083E5EBACF1E
+
+# the page includes the folders with following name convention:
+# Standard_Met_[station name]
+
+# station names are:
+# 41010 - page: https://www.ndbc.noaa.gov/station_page.php?station=41010
+# 41043 - page: https://www.ndbc.noaa.gov/station_page.php?station=41043
+# 41046 - page: https://www.ndbc.noaa.gov/station_page.php?station=41046
+# 41047 - page: https://www.ndbc.noaa.gov/station_page.php?station=41047
+# 41048 - page: https://www.ndbc.noaa.gov/station_page.php?station=41048
+# 42056 - page: https://www.ndbc.noaa.gov/station_page.php?station=42056
+# 42057 - page: https://www.ndbc.noaa.gov/station_page.php?station=42057
+# 42058 - page: https://www.ndbc.noaa.gov/station_page.php?station=42058
+# 42059 - page: https://www.ndbc.noaa.gov/station_page.php?station=42059
+# 42060 - page: https://www.ndbc.noaa.gov/station_page.php?station=42060
+
